6d2853bd078060e3f43fd0f33dddca74d489b42c
SergeyK <poshlina4@gmail.com>
2025-08-28 15:53:54 +0300

    added nvim

---
+++ nvim/lua/core/configs.lua
@@ -0,0 +1,11 @@
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.opt.clipboard = "unnamedplus"
+
+
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+vim.opt.termguicolors = true
