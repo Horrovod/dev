763c5773d3b53f6a53548031e8085f4c91be2750
deathdemise <poshlina4@gmail.com>
2025-09-04 21:07:45 +0300

    fuzzel

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,22 @@
 require("core.configs")
 require("core.mappings")
 require("core.lazy")
+vim.cmd([[
+  highlight StatusLine guibg=NONE ctermbg=NONE
+  highlight StatusLineNC guibg=NONE ctermbg=NONE
+  highlight BufferLineFill guibg=NONE ctermbg=NONE
+  highlight BufferLineBackground guibg=NONE ctermbg=NONE
+  highlight BufferLineTab guibg=NONE ctermbg=NONE
+  highlight BufferLineTabSelected guibg=NONE ctermbg=NONE
+  highlight BufferLineTabClose guibg=NONE ctermbg=NONE
+  highlight BufferLineBuffer guibg=NONE ctermbg=NONE
+  highlight BufferLineBufferSelected guibg=NONE ctermbg=NONE
+  highlight BufferLineBufferVisible guibg=NONE ctermbg=NONE
+  highlight BufferLineCloseButton guibg=NONE ctermbg=NONE
+  highlight BufferLineCloseButtonSelected guibg=NONE ctermbg=NONE
+  highlight BufferLineCloseButtonVisible guibg=NONE ctermbg=NONE
+  highlight BufferLineSeparator guibg=NONE ctermbg=NONE
+  highlight BufferLineSeparatorSelected guibg=NONE ctermbg=NONE
+  highlight BufferLineSeparatorVisible guibg=NONE ctermbg=NONE
+  highlight BufferLineIndicatorSelected guibg=NONE ctermbg=NONE
+]])
